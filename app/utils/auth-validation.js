fa683e294ebf87e021184dcac381aacd38e5c720
Georgi <g.borisov123@gmail.com>
2017-07-14 18:15:13 +0300

    Finish front-end user validation.

--- app/utils/auth-validation.js
+++ app/utils/auth-validation.js
@@ -7,15 +7,33 @@ function ensureAuthenticated(req, res, next) {
 }
 
 function frontRegisterUserValidation(req, res, next) {
+  const firstName = req.body.firstName;
+  const lastName = req.body.lastName;
   const username = req.body.username;
   const password = req.body.password;
   const email = req.body.email;
+  const phoneNumber = req.body.phoneNumber;
+  const country = req.body.country;
+  const town = req.body.town;
 
-  req.checkBody('username', 'Name is required').notEmpty();
+
+  req.checkBody('firstName', 'First name is required').notEmpty();
+  req.checkBody('firstName', 'First name must contain between 3 and 15 characters').len(3, 15);
+  req.checkBody('lastName', 'Last name is required').notEmpty();
+  req.checkBody('lastName', 'Last name must contain between 3 and 15 characters').len(3, 15);
+  req.checkBody('username', 'Username is required').notEmpty();
+  req.checkBody('username', 'Username must contain between 3 and 15 characters').len(3, 15);
   req.checkBody('password', 'Password is required').notEmpty();
+  req.checkBody('password', 'Password must contain between 3 and 15 characters').len(3, 15);
+  req.checkBody('repeatPassword', 'Passwords do not match').equals(req.body.password);
   req.checkBody('email', 'Email is required').notEmpty();
   req.checkBody('email', 'Email is not valid').isEmail();
-  req.checkBody('repeatPassword', 'Passwords do not match').equals(req.body.password);
+  req.checkBody('phoneNumber', 'Phone number is required').notEmpty();
+  req.checkBody('phoneNumber', 'Phone number must contain digits only').isInt();
+  req.checkBody('country', 'Country is required').notEmpty();
+  req.checkBody('country', 'Country must contain between 1 and 30 characters').len(1, 30);
+  req.checkBody('town', 'Town is required').notEmpty();
+  req.checkBody('town', 'Town must contain between 1 and 30 characters').len(1, 30);
 
   let errors = req.validationErrors();
 
