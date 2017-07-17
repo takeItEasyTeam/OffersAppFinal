fb2e04e0b7d6baa6552aeaca5d0de2ab216b1abc
Georgi <g.borisov123@gmail.com>
2017-07-18 01:22:13 +0300

    Reduce and Delete function from cart!

--- app/routers/cart-router.js
+++ app/routers/cart-router.js
@@ -17,15 +17,30 @@ module.exports = function(app, data) {
                     });
             
         })
-        .get('/shopingCart', function(req, res){
-            if (!req.session.cart){
-                return res.render('shopingCart-view')
+        .get('/shopingCart', function (req, res) {
+            if (!req.session.cart) {
+                return res.render('shopingCart-view');
             }
 
-            const cart = new Cart(req.session.cart)
+            const cart = new Cart(req.session.cart);
             res.render('shopingCart-view', { products: cart.generateArray(), totalPrice: cart.totalPrice })
         })
+        .get('/reduce/:id', function(req, res, next) {
+            const productId = req.params.id;
+            const cart = new Cart(req.session.cart ? req.session.cart : {});
 
+            cart.reduceByOne(productId);
+            req.session.cart = cart;
+            res.redirect('/shopingCart');
+        })
+        .get('/remove/:id', function(req, res, next) {
+            const productId = req.params.id;
+            const cart = new Cart(req.session.cart ? req.session.cart : {});
+
+            cart.removeItem(productId);
+            req.session.cart = cart;
+            res.redirect('/shopingCart');
+        })
 
     app.use('/', router);
 };
