a8cbf470136e7f0db763ee9e4b3a9d56d5fa52db
Georgi <g.borisov123@gmail.com>
2017-07-25 12:04:18 +0300

    Back to the top button.

--- static/js/main.js
+++ static/js/main.js
@@ -1,34 +1,34 @@
-$(document).ready(function(){
-  $('.delete-offer').on('click', function(e){
+$(document).ready(function () {
+  $('.delete-offer').on('click', function (e) {
     $target = $(e.target);
     const id = $target.attr('data-id');
     $.ajax({
       type: 'DELETE',
-      url: '/' +id,
-      success: function(response) {
-        window.location.href='/profile/myOffers';
+      url: '/' + id,
+      success: function (response) {
+        window.location.href = '/profile/myOffers';
       },
-      error: function(err) {
+      error: function (err) {
         console.log(err);
       },
     });
   });
-  $('.buy').on('click', function(e){
+  $('.buy').on('click', function (e) {
     $target = $(e.target);
     const id = $target.attr('data-id');
     $.ajax({
       type: 'GET',
-      url: '/addToCart/' +id,
-      success: function(response) {
+      url: '/addToCart/' + id,
+      success: function (response) {
         $('.badge').html(response);
       },
-      error: function(err) {
+      error: function (err) {
         console.log(err);
       },
     });
   });
-  $('#sortByPrice-High').on('click', function(e){
-    const currentPath ={
+  $('#sortByPrice-High').on('click', function (e) {
+    const currentPath = {
       path: window.location.pathname,
       order: -1,
     };
@@ -36,16 +36,16 @@ $(document).ready(function(){
       type: 'POST',
       url: '/sort',
       data: currentPath,
-      success: function(response) {
+      success: function (response) {
         getTemplateAjax(response);
       },
-      error: function(err) {
+      error: function (err) {
         console.log(err);
       },
     });
   });
-  $('#sortByPrice-Low').on('click', function(e){
-    const currentPath ={
+  $('#sortByPrice-Low').on('click', function (e) {
+    const currentPath = {
       path: window.location.pathname,
       order: 1,
     };
@@ -53,10 +53,10 @@ $(document).ready(function(){
       type: 'POST',
       url: '/sort',
       data: currentPath,
-      success: function(response) {
+      success: function (response) {
         getTemplateAjax(response);
       },
-      error: function(err) {
+      error: function (err) {
         console.log(err);
       },
     });
@@ -69,23 +69,39 @@ $(document).ready(function(){
     let test;
 
     $.ajax({
-        url: `/static/templates/testovo.handlebars`,
-        cache: true,
-        success: function (data) {
-            source = data;
-            template = Handlebars.compile(source);
-            test = template(dataFromApi);
-            $('.proba').html(test);
-            setTimeout(function(){ 
-              $('.loader').hide(); 
-            }, 0);
-            
-        }
+      url: `/static/templates/testovo.handlebars`,
+      cache: true,
+      success: function (data) {
+        source = data;
+        template = Handlebars.compile(source);
+        test = template(dataFromApi);
+        $('.proba').html(test);
+        setTimeout(function () {
+          $('.loader').hide();
+        }, 0);
+
+      }
     });
   };
-  
+
+  //back to the top
+    window.onscroll = function() {scrollFunction()};
+
+  function scrollFunction() {
+      if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+          document.getElementById("myBtn").style.display = "block";
+      } else {
+          document.getElementById("myBtn").style.display = "none";
+      }
+  }
+
+  $('#myBtn').on('click', function(e){
+    document.body.scrollTop = 0;  
+    document.documentElement.scrollTop = 0; 
+  })
+
   //test
-  $('#btn-add-message').on('click', function(e){
+  $('#btn-add-message').on('click', function (e) {
     $('#message-form').toggleClass('hidden');
     $('#btn-add-message').toggleClass('btn-success');
   });
@@ -93,11 +109,11 @@ $(document).ready(function(){
   $(function () {
     let dateNow = new Date();
     $('#datetimepicker').datetimepicker({
-      locale: 'bg', 
+      locale: 'bg',
       defaultDate: moment(dateNow).hours(23).minutes(59).seconds(59).milliseconds(0)
     });
     $('#datetimepicker2').datetimepicker({
-      locale: 'bg' 
+      locale: 'bg'
     });
   });
 });
