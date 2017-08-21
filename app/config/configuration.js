1401182ed6f78577c200ac4d60d8f39d2bf34137
Viliana Zhilkova <vilianazhilkova@abv.bg>
2017-08-21 18:35:51 +0300

    Changes in config/configuration.js

--- app/config/configuration.js
+++ app/config/configuration.js
@@ -1,9 +1,16 @@
 /* globals process */
 
-const port = 3001;
-const connectionString = 'mongodb://localhost/OffersDB';
+const port = process.env.PORT || 3001;
+const connectionStrings = {
+    production: process.env.CONNECTION_STRING,
+    development: 'mongodb://localhost/OffersDB',
+};
+
     // 'mongodb://localhost/OffersDB', // local usage
     // 'mongodb://mongo:27017/', // Docker usage
     // 'mongodb://35.158.233.58:27017/', // AWS usage
 
-module.exports = { connectionString, port };
+module.exports = {
+    connectionString: connectionStrings[process.env.NODE_ENV || 'development'],
+    port,
+};
